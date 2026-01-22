60c37e83b66de310e815dc38d6b0699b0aa85e10
omryakawar <omryakawar@gmail.com>
2026-01-22 18:56:59 +0530

    k8s and terraform

--- terraform/modules/addons/alb-controller.tf
+++ terraform/modules/addons/alb-controller.tf
@@ -1,17 +1,27 @@
 resource "helm_release" "alb" {
   name       = "aws-load-balancer-controller"
   namespace  = "kube-system"
+
   repository = "https://aws.github.io/eks-charts"
   chart      = "aws-load-balancer-controller"
 
-  set = [
-    {
-      name  = "clusterName"
-      value = var.cluster_name
-    },
-    {
-      name  = "region"
-      value = var.region
-    }
-  ]
+  set {
+    name  = "clusterName"
+    value = var.cluster_name
+  }
+
+  set {
+    name  = "region"
+    value = var.region
+  }
+
+  set {
+    name  = "serviceAccount.create"
+    value = "false"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
 }
--- terraform/modules/addons/cluster-autoscaler.tf
+++ terraform/modules/addons/cluster-autoscaler.tf
@@ -1,17 +1,27 @@
 resource "helm_release" "autoscaler" {
   name       = "cluster-autoscaler"
   namespace  = "kube-system"
+
   repository = "https://kubernetes.github.io/autoscaler"
   chart      = "cluster-autoscaler"
 
-  set = [
-    {
-      name  = "autoDiscovery.clusterName"
-      value = var.cluster_name
-    },
-    {
-      name  = "awsRegion"
-      value = var.region
-    }
-  ]
+  set {
+    name  = "autoDiscovery.clusterName"
+    value = var.cluster_name
+  }
+
+  set {
+    name  = "awsRegion"
+    value = var.region
+  }
+
+  set {
+    name  = "rbac.serviceAccount.create"
+    value = "false"
+  }
+
+  set {
+    name  = "rbac.serviceAccount.name"
+    value = "cluster-autoscaler"
+  }
 }
